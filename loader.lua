0caeb228453a2b1c7c9997c03e0b6d20736fe697
dlt <idk607620@gmail.com>
2025-03-04 14:00:35 -0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,6 +2,4 @@ if game.PlaceId == "16732694052" then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/ImNotRox1/Trevous-Hub/refs/heads/main/fisch.lua"))()
 elseif game.PlaceId == "13772394625" then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/ImNotRox1/Trevous-Hub/refs/heads/main/blade-ball.lua"))()
-elseif game.PlaceId ~= "13772394625" or "16732694052" then
-  game.Players.LocalPlayer:Kick("Game not supported.")
 end
