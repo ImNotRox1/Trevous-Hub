e240abef5fd3b855b19e2e93663ea462408de588
dlt <idk607620@gmail.com>
2025-03-04 13:04:18 -0300

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,7 @@
+if game.PlaceId == "16732694052" then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/ImNotRox1/Trevous-Hub/refs/heads/main/fisch.lua"))()
+elseif game.PlaceId == "13772394625" then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/ImNotRox1/Trevous-Hub/refs/heads/main/blade-ball.lua"))()
+elseif game.PlaceId ~= "13772394625" or "16732694052" then
+  return
+end
